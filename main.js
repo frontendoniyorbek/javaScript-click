3cd5bae810d9fcc29c113a80ed0e94be5a7db7b3
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2024-07-13 09:40:07 +0500

    add click

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const elList = document.querySelector('.list');
 const elItim = document.querySelectorAll('.item');
 const elBtn = document.querySelector('.btn');
-elBtn.addEventListener('click', () => {
-	console.log(elItim);
-});
 
 elItim.forEach(item => {
 	item.addEventListener('click', e => {
 		e.target.style.color = 'red';
+		e.target.style.backgroundColor = 'violet';
 	});
+});
+
+elBtn.addEventListener('click', () => {
+	console.log(elItim);
 });
